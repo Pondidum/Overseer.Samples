b90046d1e8a042c30af77ebd6546ac34eda12127
andy <andy@andydote.co.uk>
2015-08-25 18:10:25 +0100

    samples: add testing.posttest.xunit

---
+++ Testing.PostTest.Xunit/Events/CandidateCreated.cs
@@ -0,0 +1,9 @@
+﻿namespace Testing.PostTest.Xunit.Events
+{
+	public class CandidateCreated
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+	}
+}
