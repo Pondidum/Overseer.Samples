b3613d26870cb510e4236b116a309235cd34a61e
andy <andy@andydote.co.uk>
2015-08-28 17:55:10 +0100

    console.rabbit: add colour!

--- Console.Rabbit/ConsoleValidationOutput.cs
+++ Console.Rabbit/ConsoleValidationOutput.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Overseer;
 
@@ -6,10 +7,25 @@ namespace Console.Rabbit
 {
 	internal class ConsoleValidationOutput : IValidationOutput
 	{
+		private readonly Dictionary<Status, ConsoleColor> _colorMap;
+
+		public ConsoleValidationOutput()
+		{
+			_colorMap = new Dictionary<Status, ConsoleColor>
+			{
+				{ Status.Pass, ConsoleColor.Green},
+				{ Status.NotInterested, System.Console.ForegroundColor },
+				{ Status.Warning, ConsoleColor.Yellow },
+				{ Status.Fail, ConsoleColor.Red }
+			};
+		}
 		public void Write(ValidationResult result)
 		{
 			var messages = BuildMessageLine(result);
-			System.Console.WriteLine("{0}: {1}", result.Status, string.Join(", ", messages));
+
+			System.Console.ForegroundColor = _colorMap[result.Status];
+            System.Console.WriteLine("{0}: {1}", result.Status, string.Join(", ", messages));
+			System.Console.ResetColor();
 		}
 
 		private IEnumerable<string> BuildMessageLine(ValidationResult result)
